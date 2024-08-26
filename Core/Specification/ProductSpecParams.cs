e31941ae873c90653c9d2ebc8792e66fd9f5bc33
maheshvp <maheshvp.cs@gmail.com>
2024-08-26 22:19:38 +0530

    Add search functionality

--- Core/Specification/ProductSpecParams.cs
+++ Core/Specification/ProductSpecParams.cs
@@ -37,4 +37,11 @@ public class ProductSpecParams
     }
 
     public string? Sort  { get; set; }
+
+    private string? _search;
+    public string Search
+    {
+        get => _search ?? "";
+        set => _search = value.ToLower();
+    }    
 }
